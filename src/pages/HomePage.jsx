fa502844bc4d4bc2eaa0d26f8ae68e5feb03a642
marcoroncuzzi02 <marcoroncuzzi02@gmail.com>
2025-03-25 23:34:38 +0100

    ADD cors

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,33 @@
 import MoovieCard from "../components/MoovieCard"
+import axios from 'axios'
+import { useState, useEffect } from "react"
 
 export default function HomePage(){
+
+    const [movies,setMovies] = useState( [] )
+
+    const fetchMoovies = () => {
+        console.log ( 'fetching moovies')
+
+        axios.get('http://localhost:3005/movies')
+        .then( res => { setMovies( res.data ) } )
+        .catch( error => {console.log(error)})
+    }
+
+    const renderMovies = () => {
+        return(
+            movies.map( (movie) => {
+                return(
+                    <div className="col" key={book.id}>
+                        <BookCard movie={movie}/>
+                    </div>
+                )
+            })
+        )
+    }
+
+    useEffect( fetchMoovies, [] )
+
     return(
         <>
             <h1 className="text-primary">Bool Books</h1>
